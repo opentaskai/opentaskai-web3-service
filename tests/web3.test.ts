2f0bdb99ff72fc5cecdd629c6757fd2c84e0c2a4
tercel <tercel.yi@gmail.com>
2024-05-31 11:09:29 +0800

    update opentaskai-web3-jssdk

--- tests/web3.test.ts
+++ tests/web3.test.ts
@@ -4,59 +4,59 @@ import { uuid } from '../src/utils/util';
 import { APP_ENV } from '../src/constants';
 
 describe('Web3', async () => {
-    describe('base', async () => {
-        let res: any;
-        const chain = getChain(APP_ENV.CHAIN_ID);
-        const payment = getPayment(chain);
-        const nft = getAiGenesis(chain);
-        const expired = Math.floor(Date.now() / 1000) + 300;
+    // describe('base', async () => {
+    let res: any;
+    const chain = getChain(APP_ENV.CHAIN_ID);
+    const payment = getPayment(chain);
+    const nft = getAiGenesis(chain);
+    const expired = Math.floor(Date.now() / 1000) + 300;
 
-        before(async () => {
-            console.log('chain id:', chain.chainId);
-            console.log('payment address:', payment.address);
-        });
-
-        after(async () => {});
-
-        it('getBalance', async () => {
-            console.log('getBalance ...', constants.ZERO_ADDRESS);
-            res = await payment.getBalance(constants.ZERO_ADDRESS + '');
-            console.log('getBalance:', res);
-        });
-
-        it('signDepositData', async () => {
-            const to = uuid();
-            const token = chain.getTokenAddr('USDT');
-            console.log('usdt', token);
-            const available = common.bignumber.bnWithDecimals(2, 6);
-            const frozen = common.bignumber.bnWithDecimals(1, 6);
-            const sn = uuid();
-            const data = await payment.signDepositData(to, token, available, frozen, sn, expired);
-            console.log('getBalance:', data);
-        });
-
-        it('signCancelData', async () => {
-            const userA: any = {
-                user: '0xBe46A6c57aB6d9272b5674C47fe587Dd3B5B54Db',
-                token: chain.getTokenAddr('USDT'),
-                amount: common.bignumber.bnWithDecimals(2, 18),
-                fee: common.bignumber.bnWithDecimals(1, 18)
-            };
-            const userB: any = {
-                user: await chain.getAccount(),
-                token: chain.getTokenAddr('USDT'),
-                amount: common.bignumber.bnWithDecimals(2, 18),
-                fee: common.bignumber.bnWithDecimals(1, 18)
-            };
-            const sn = uuid();
-            const data = await payment.signCancelData(userA, userB, sn, expired);
-            console.log('signCancelData:', data);
-        });
-
-        it('signMintData', async () => {
-            const sn = uuid();
-            const data = await nft.signMintData(sn, expired);
-            console.log('signMintData:', data);
-        });
+    before(async () => {
+        console.log('chain id:', chain.chainId);
+        console.log('payment address:', payment.address);
     });
+
+    after(async () => {});
+
+    it('getBalance', async () => {
+        console.log('getBalance ...', constants.ZERO_ADDRESS);
+        res = await payment.getBalance(constants.ZERO_ADDRESS + '');
+        console.log('getBalance:', res);
+    });
+
+    it('signDepositData', async () => {
+        const to = uuid();
+        const token = chain.getTokenAddr('USDT');
+        console.log('usdt', token);
+        const available = common.bignumber.bnWithDecimals(2, 6);
+        const frozen = common.bignumber.bnWithDecimals(1, 6);
+        const sn = uuid();
+        const data = await payment.signDepositData(to, token, available, frozen, sn, expired);
+        console.log('getBalance:', data);
+    });
+
+    it('signCancelData', async () => {
+        const userA: any = {
+            user: '0xBe46A6c57aB6d9272b5674C47fe587Dd3B5B54Db',
+            token: chain.getTokenAddr('USDT'),
+            amount: common.bignumber.bnWithDecimals(2, 18),
+            fee: common.bignumber.bnWithDecimals(1, 18)
+        };
+        const userB: any = {
+            user: await chain.getAccount(),
+            token: chain.getTokenAddr('USDT'),
+            amount: common.bignumber.bnWithDecimals(2, 18),
+            fee: common.bignumber.bnWithDecimals(1, 18)
+        };
+        const sn = uuid();
+        const data = await payment.signCancelData(userA, userB, sn, expired);
+        console.log('signCancelData:', data);
+    });
+
+    it('signMintData', async () => {
+        const sn = uuid();
+        const data = await nft.signMintData(sn, expired);
+        console.log('signMintData:', data);
+    });
+    // });
 });
