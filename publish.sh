868f8f4781f8d6e03e6f4b3f9b8770acbd0f2054
tercel <tercel.yi@gmail.com>
2024-08-07 14:05:27 +0800

    add publish.sh

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+FULL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$FULL_DIR" || exit
+echo `pwd`
+DIR_NAME=$(basename "$FULL_DIR")
+echo "current folder: $DIR_NAME"
+APP_NAME=${DIR_NAME}
+echo "app name: $APP_NAME"
+
+git pull
+#rm -rf .next
+pnpm install
+pnpm swagger
+pnpm run build
+echo "pm2 restart $APP_NAME"
+pm2 restart $APP_NAME
