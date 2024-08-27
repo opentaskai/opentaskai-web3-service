579505e90a6cee39649b4b8a4f2072dd6e4ccdfd
tercel <tercel.yi@gmail.com>
2024-08-27 17:33:43 +0800

    update check-send

--- src/command/check-send.ts
+++ src/command/check-send.ts
@@ -3,11 +3,14 @@ import {  CHAIN_IDS } from '../constants';
 import { getTranscationCount } from '../services/metrics';
 
 async function handle(_chainId: number) {
-    const {CHECK_ENV, chainId, account, latest, pending } = await getTranscationCount(_chainId);
+    const {CHECK_ENV, chainId, account, balance, latest, pending } = await getTranscationCount(_chainId);
     console.log({CHECK_ENV, chainId, account, latest, pending });
     const count = pending - latest;
     if (count > CHECK_ENV.SEND_PENDING && CHECK_ENV.EMAIL_RECEIVER) {
         const res = await mail.sendMail('OpenTaskAI no-reply@opentaskai.com', CHECK_ENV.EMAIL_RECEIVER, `${count} pending transactions`, 'There are too many transactions waiting to be processed and it is recommended that the cause of the problem be identified.', 'warning');
+        console.log('sendMail res:', res);
+    } else if (balance < 0.2) {
+        const res = await mail.sendMail('OpenTaskAI no-reply@opentaskai.com', CHECK_ENV.EMAIL_RECEIVER, `balance is running low: ${balance}`, 'The balance is running low, please recharge in time!', 'warning');
         console.log('sendMail res:', res);
     }
 }
--- src/services/metrics.ts
+++ src/services/metrics.ts
@@ -1,15 +1,19 @@
 import { getClearPayment } from '../web3';
 import { CHECK_ENV } from '../constants';
+import { BigNumber } from 'bignumber.js';
 
 export async function getTranscationCount(chainId: number) {
     const payment = getClearPayment(chainId);
     const account =  await payment.chain.getAccount();
     const latest = await payment.chain.getTransactionCount(account);
     const pending = await payment.chain.getTransactionCount(account, 'pending');
+    const amount = await payment.chain.getBalance();
+    const balance = BigNumber(amount).shiftedBy(-18).toNumber();
     return {
         CHECK_ENV,
         chainId,
         account,
+        balance,
         latest,
         pending
     };
